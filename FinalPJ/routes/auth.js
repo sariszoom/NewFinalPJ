9f7ee80d80a45a5cbe36d00eb41940ca4f95045e
sariszoom <sariszoom@gmail.comuyuftytyddtydtyfyufyufyuftyudtdghdghdfdfgdfffhjftyffdrtdtysrtdtydtydfrtsddghfghsddghhjghfghjgfghjfghfghlgkl;jdfklgjfkldgjldfkjglkfjgh;lkdgjhlkjfgf;dkds;fkjhjihkdfkjfdigjiodfjgl;lkpso>
2023-02-12 19:12:46 +0700

    register to mongoDB

--- FinalPJ/routes/auth.js
+++ FinalPJ/routes/auth.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+
 const router = express.Router();
 const User = require('../model/user')
 
-router.post('/register', (req, res) => {
-  console.log(req.body);
-  res.redirect('/');
+router.post('/register', async (req, res) => {
+  const { username, password, name } = req.body;
+
+  // simple validation
+  if (!name || !username || !password) {
+    return res.render('register', { message: 'Please try again' });
+  }
+
+  const passwordHash = bcrypt.hashSync(password, 10);
+  const user = new User({
+    name,
+    username,
+    password: passwordHash
+  });
+  await user.save();
+  res.render('index', { user });
 });
 
 router.post('/login', async (req, res) => {
